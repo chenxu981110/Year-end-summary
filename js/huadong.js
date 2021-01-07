eda326e1d8c4fd99a4dbee1cd71f8eacbb1ffc6a
chenxu981110 <1845042363@qq.com>
2021-01-07 17:14:37 +0800

    first commit

---
+++ js/huadong.js
@@ -0,0 +1,17 @@
+    /*<script src="http://www.lanrenzhijia.com/ajaxjs/1.10.1/jquery-1.10.1.min.js"></script>
+<script src="js/flowup.js"></script>
+<script>
+    $(function () {
+        $("body").flowUp("#module1", { transalteY: 350, duration: 1 });
+        $("body").flowUp("#module2", { transalteY: 350, duration: 1 });
+        $("body").flowUp("#module3", { transalteY: 350, duration: 1 });
+        $("body").flowUp(".module4", { transalteY: 350, duration: 1 });
+        $("body").flowUp(".module5", { transalteY: 350, duration: 1 });
+        $("body").flowUp(".module6", { transalteY: 350, duration: 1 });
+        $("body").flowUp(".module7", { transalteY: 350, duration: 1 });
+        $("body").flowUp(".module8", { transalteY: 350, duration: 1 });
+        $("body").flowUp("#module6", { transalteY: 350, duration: 1 });
+        $("body").flowUp("#module7", { transalteY: 350, duration: 1 });
+
+    });
+</script>*/
